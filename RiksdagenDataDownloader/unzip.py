1ddcca6b9642a301e253bab1703fae7253a52080
Rickard <rickard.lofberg@gmail.com>
2018-04-30 21:10:39 +0200

    Verbose + zipparsing fixed
    - More verbose if wanted
    - Error with missing lines in files fixed

--- RiksdagenDataDownloader/unzip.py
+++ RiksdagenDataDownloader/unzip.py
@@ -1,6 +1,6 @@
 import zipfile, io, os
 
-def yield_zip_content( request_content ):
+def yield_zip_content( request_content, verbose=False):
     """ Returns the raw data as a string from requests object
     which is a zipfile. """
     # Request_content would be the same as request_obj.content
@@ -14,6 +14,10 @@ def yield_zip_content( request_content ):
 
     # Go through each zipped file
     for zipped_file in files:
+        # Print out the name of zipfile
+        if verbose:
+            print("Unpacking {}".format(zipped_file))
+        
         # Read the bytes in that file
         byte_data = io.BytesIO(zipdata.read(zipped_file))
 
@@ -21,25 +25,34 @@ def yield_zip_content( request_content ):
         text_data = ''
 
         # Read all the lines in the data
-        while byte_data.readline():
-            # Byte to text
-            text_data += byte_data.readline().decode('utf-8')
+        for line in byte_data:
+            # Decode to UTF-8
+            text_data += line.decode('utf-8')
 
         # Yield the data for each zipped file one at a time
         yield text_data
 
-def save_zip_content( request_content, directory='' ):
+def save_zip_content(request_content, directory='', verbose=False):
     """ Saves the content of the zipfile to path """
 
     # Make sure we have the directory to save to
     if not os.path.exists(directory):
         os.makedirs(directory)
+        if verbose:
+            print("Created directory: {}".format(directory))
 
     # Read the Bytes into a ZipFile-Object
     zipdata = zipfile.ZipFile(io.BytesIO(request_content))
 
+    if verbose:
+        print("Starting to extract zipfiles")
+    
     # There can be more than one file ...
     for file_name in zipdata.namelist():
-        #.os.path.join(dir_name, base_filename 
+        
+
         # Extract the file
         zipdata.extract(file_name, path=directory)
+
+    if verbose:
+        print("Extraction complete")
